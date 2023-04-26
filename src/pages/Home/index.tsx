27c35bf16977dcb64a0ef7350e506fba3191450f
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-04-26 19:33:46 -0300

    personalização da tela home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,8 +2,19 @@ import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 export function Home() {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={styles.container}>
+      <Text style={styles.textHome}>Rick and Morty</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#202020",
+    flex: 1,
+    alignItems: "center",
+  },
+  textHome: { color: "#8bcf21", marginTop: "30%" },
+});
+
+// como chamar api em uma lista?
