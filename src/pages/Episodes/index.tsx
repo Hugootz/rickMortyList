63a7dbcc54ab6b336d027c3cf9ee2f1f4105c12c
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-04-26 19:34:56 -0300

    personalização tela episodios

--- src/pages/Episodes/index.tsx
+++ src/pages/Episodes/index.tsx
@@ -1,9 +1,18 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
 export function Episodes() {
   return (
-    <View>
-      <Text>Episodes</Text>
+    <View style={styles.container}>
+      <Text style={styles.textEpisodes}>Episodes</Text>
     </View>
   );
 }
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#202020",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textEpisodes: { color: "#8bcf21" },
+});
