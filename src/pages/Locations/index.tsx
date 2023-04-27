f969dd8f75ecfb75669cc8cbb6da8f0852425799
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-04-27 19:38:04 -0300

    adição de fonte e stylesheet em localizações

--- src/pages/Locations/index.tsx
+++ src/pages/Locations/index.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts, Creepster_400Regular } from "@expo-google-fonts/creepster";
 export function Locations() {
+  SplashScreen.preventAutoHideAsync();
+  const [fontsLoaded] = useFonts({ Creepster_400Regular });
+  if (!fontsLoaded) {
+    return null;
+  }
+  SplashScreen.hideAsync();
   return (
     <View style={styles.container}>
-      <Text style={styles.textLocations}>Locations</Text>
+      <Text style={styles.textLocations}>Localizações</Text>
     </View>
   );
 }
@@ -12,7 +20,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#202020",
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
   },
-  textLocations: { color: "#8bcf21" },
+  textLocations: {
+    color: "#8bcf21",
+    fontFamily: "Creepster_400Regular",
+    marginTop: "25%",
+    fontSize: 35,
+    marginRight: "45%",
+    top: -10,
+  },
 });
