dc87cb6941fc338ea0d96531a1c8c961cd069b8f
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-04-27 19:40:57 -0300

    adição de fonte stylesheet em episodes

--- src/pages/Episodes/index.tsx
+++ src/pages/Episodes/index.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts, Creepster_400Regular } from "@expo-google-fonts/creepster";
 export function Episodes() {
+  SplashScreen.preventAutoHideAsync();
+  const [fontsLoaded] = useFonts({ Creepster_400Regular });
+  if (!fontsLoaded) {
+    return null;
+  }
+  SplashScreen.hideAsync();
   return (
     <View style={styles.container}>
-      <Text style={styles.textEpisodes}>Episodes</Text>
+      <Text style={styles.textEpisodes}>Episódios</Text>
     </View>
   );
 }
@@ -12,7 +20,13 @@ const styles = StyleSheet.create({
     backgroundColor: "#202020",
     flex: 1,
     alignItems: "center",
-    justifyContent: "center",
   },
-  textEpisodes: { color: "#8bcf21" },
+  textEpisodes: {
+    color: "#8bcf21",
+    fontFamily: "Creepster_400Regular",
+    marginTop: "25%",
+    fontSize: 35,
+    marginRight: "55%",
+    top: -10,
+  },
 });
