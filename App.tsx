a962078d27656bcb6c4752f87fc100641fb0e46b
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-05-04 10:57:24 -0300

    fonte global para o app

--- App.tsx
+++ App.tsx
@@ -2,7 +2,15 @@ import React from "react";
 import AppRoutes from "./src/Routes/app.Routes";
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "react-native";
+import * as SplashScreen from "expo-splash-screen";
+import { useFonts, Creepster_400Regular } from "@expo-google-fonts/creepster";
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+  const [fontsLoaded] = useFonts({ Creepster_400Regular });
+  if (!fontsLoaded) {
+    return null;
+  }
+  SplashScreen.hideAsync();
   return (
     <NavigationContainer>
       <StatusBar backgroundColor="#202020" translucent={true} />
