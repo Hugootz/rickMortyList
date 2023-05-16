f3bd2720b59d6da43ada128d305608eb3911b71f
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-05-16 10:56:31 -0300

    adição icone favoritos e css

--- src/components/FavoriteModal.tsx
+++ src/components/FavoriteModal.tsx
@@ -1,11 +1,27 @@
 import React from "react";
-import { SafeAreaView, View, Text, StyleSheet } from "react-native";
+import {
+  SafeAreaView,
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { Octicons } from "@expo/vector-icons";
 
 export function FavoriteModal() {
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
         <Text style={styles.modalText}>favoritos</Text>
+      </View>
+
+      <View style={styles.iconView}>
+        <Octicons
+          style={styles.icon}
+          name="feed-star"
+          size={30}
+          color="#8bcf21"
+        />
       </View>
     </SafeAreaView>
   );
@@ -15,18 +31,26 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#202020",
-
-    alignItems: "center",
+    textAlign: "center",
   },
 
   content: {
-    marginTop: "15%",
-    marginRight: "53%",
-    top: -10,
+    flex: 1,
+    backgroundColor: "#202020",
   },
+  iconView: {
+    flex: 5,
+    backgroundColor: "#3f3f3f",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    elevation: 50,
+  },
+  icon: { alignSelf: "center", top: -40, right: 20 },
   modalText: {
     color: "#8bcf21",
     fontFamily: "Creepster_400Regular",
     fontSize: 35,
+    top: 75,
+    margin: 10,
   },
 });
