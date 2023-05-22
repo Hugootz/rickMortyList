4788f1187783141d119b433c56ce923e5b4b2bef
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-05-22 12:40:48 -0300

    tela interna personagens

---
+++ src/pages/Characters/CharactersList/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+
+export function InternalList() {
+  return (
+    <View style={styles.container}>
+      <Text>Internal List</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
