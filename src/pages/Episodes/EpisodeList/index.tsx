cd7f99adc24eedbdbf9014a3fae19d3ee1c3b2d4
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-05-25 12:02:41 -0300

    adição do botão de voltar e estilização

--- src/pages/Episodes/EpisodeList/index.tsx
+++ src/pages/Episodes/EpisodeList/index.tsx
@@ -1,11 +1,26 @@
 import React from "react";
-import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { Entypo } from "@expo/vector-icons";
 
-export function EpisodeList() {
+export function EpisodeList({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>Episode List</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity
+        style={styles.episodeIcon}
+        onPress={() => navigation.goBack()}
+      >
+        <Entypo name="back" size={35} color="#8bcf21" />
+      </TouchableOpacity>
+      <View style={styles.episodeView}>
+        <Text style={styles.episodeText}>Episode List</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
@@ -13,7 +28,21 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
 
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#202020",
   },
+  episodeIcon: {
+    flex: 1,
+    backgroundColor: "#202020",
+    top: 70,
+    alignSelf: "flex-start",
+    marginLeft: 20,
+  },
+  episodeView: {
+    flex: 3,
+    backgroundColor: "#3f3f3f",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    elevation: 80,
+  },
+  episodeText: { color: "#8bcf21", top: 80, alignSelf: "center" },
 });
