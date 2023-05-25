966b9d0626978b1cf2e676faec98a8829744d04a
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-05-25 11:59:31 -0300

    introdução ao botão de voltar e estilização

--- src/pages/Locations/LocationList/index.tsx
+++ src/pages/Locations/LocationList/index.tsx
@@ -1,11 +1,24 @@
 import React from "react";
-import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { Entypo } from "@expo/vector-icons";
 
-export function LocationList() {
+export function LocationList({ navigation }) {
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <Text>Location List</Text>
+    <SafeAreaView style={styles.container}>
+      <TouchableOpacity
+        style={styles.locationIcon}
+        onPress={() => navigation.goBack()}
+      >
+        <Entypo name="back" size={35} color="#8bcf21" />
+      </TouchableOpacity>
+      <View style={styles.locationView}>
+        <Text style={styles.locationText}>Location List</Text>
       </View>
     </SafeAreaView>
   );
@@ -14,8 +27,21 @@ export function LocationList() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: "#202020",
   },
+  locationIcon: {
+    flex: 1,
+    backgroundColor: "#202020",
+    top: 70,
+    alignSelf: "flex-start",
+    marginLeft: 20,
+  },
+  locationView: {
+    flex: 3,
+    backgroundColor: "#3f3f3f",
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    elevation: 80,
+  },
+  locationText: { color: "#8bcf21", top: 80, alignSelf: "center" },
 });
