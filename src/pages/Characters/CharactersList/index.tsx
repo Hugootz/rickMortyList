d06ae65dbba73f358af7e65b2825e680b7037593
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-05-29 20:03:41 -0300

    mudança css

--- src/pages/Characters/CharactersList/index.tsx
+++ src/pages/Characters/CharactersList/index.tsx
@@ -65,6 +65,8 @@ const styles = StyleSheet.create({
   charactersView: {
     flex: 3,
     backgroundColor: "#3f3f3f",
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
 
     elevation: 80,
     alignItems: "center",
@@ -75,7 +77,13 @@ const styles = StyleSheet.create({
     alignItems: "center",
     elevation: 50,
   },
-  characterImage: { width: 200, height: 200, borderRadius: 25 },
+  characterImage: {
+    width: 200,
+    height: 200,
+    borderRadius: 25,
+    borderWidth: 3,
+    borderColor: "#8bcf21",
+  },
   rickText: {
     color: "#fff",
     fontSize: 20,
@@ -83,12 +91,14 @@ const styles = StyleSheet.create({
   },
   rickInfo: {
     top: 200,
-    width: 250,
+    width: 350,
     height: 250,
     backgroundColor: "#232323",
     borderRadius: 30,
     justifyContent: "center",
     margin: 10,
+    borderWidth: 2,
+    borderColor: "#8bcf21",
   },
   textId: {
     color: "#8bcf21",
