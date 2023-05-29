ebc135acd9e87cb39fedabf4d9fd410e4a9aa12b
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-05-29 18:03:44 -0300

    transferência de dados para outra tela

--- src/pages/Characters/CharactersList/index.tsx
+++ src/pages/Characters/CharactersList/index.tsx
@@ -13,12 +13,12 @@ import { useRoute } from "@react-navigation/native";
 import { CharactersCard } from "../../Characters";
 
 interface Params {
-  id: CharactersCard;
+  character: CharactersCard;
 }
 
 export function CharactersList({ navigation }) {
   const route = useRoute();
-  const { id } = route.params as Params;
+  const { character } = route.params as Params;
 
   return (
     <SafeAreaView style={styles.container}>
@@ -29,8 +29,19 @@ export function CharactersList({ navigation }) {
         <Entypo name="back" size={35} color="#8bcf21" />
       </TouchableOpacity>
       <View style={styles.charactersView}>
-        <Image source={{}} />
-        <Text></Text>
+        <View style={styles.rickView}>
+          <Image
+            style={styles.characterImage}
+            source={{ uri: character.image }}
+          />
+          <Text style={styles.rickText}>{character.name}</Text>
+        </View>
+        <View style={styles.rickInfo}>
+          <Text>{character.id}</Text>
+          <Text>{character.status}</Text>
+          <Text>{character.species}</Text>
+          <Text>{character.gender}</Text>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -52,10 +63,22 @@ const styles = StyleSheet.create({
   charactersView: {
     flex: 3,
     backgroundColor: "#3f3f3f",
-    borderTopLeftRadius: 25,
-    borderTopRightRadius: 25,
+
     elevation: 80,
     alignItems: "center",
   },
+  rickView: {
+    position: "absolute",
+    top: -100,
+    alignItems: "center",
+    elevation: 50,
+  },
+  characterImage: { width: 200, height: 200, borderRadius: 25 },
+  rickText: {
+    color: "#fff",
+    fontSize: 20,
+    fontWeight: "bold",
+  },
+  rickInfo: { alignSelf: "center", top: 200 },
   charactersText: { color: "#8bcf21", top: 80, alignSelf: "center" },
 });
