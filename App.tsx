4cd4e6eed84abaf002092440cc7e2d4d289a824d
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-05-29 18:04:43 -0300

    add

--- App.tsx
+++ App.tsx
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "react-native";
 import * as SplashScreen from "expo-splash-screen";
 import { useFonts, Creepster_400Regular } from "@expo-google-fonts/creepster";
+
 export default function App() {
   SplashScreen.preventAutoHideAsync();
   const [fontsLoaded] = useFonts({ Creepster_400Regular });
