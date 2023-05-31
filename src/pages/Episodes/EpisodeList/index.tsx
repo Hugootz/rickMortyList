37592e2b36c94d3a4d3f89c17bebd926db9927af
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-05-31 08:54:33 -0300

    troca de info para tela episodeList

--- src/pages/Episodes/EpisodeList/index.tsx
+++ src/pages/Episodes/EpisodeList/index.tsx
@@ -7,8 +7,16 @@ import {
   TouchableOpacity,
 } from "react-native";
 import { Entypo } from "@expo/vector-icons";
+import { useRoute } from "@react-navigation/native";
+import { EpisodeCard } from "../../Episodes";
+
+interface Params {
+  episode: EpisodeCard;
+}
 
 export function EpisodeList({ navigation }) {
+  const route = useRoute();
+  const { episode } = route.params as Params;
   return (
     <SafeAreaView style={styles.container}>
       <TouchableOpacity
@@ -17,8 +25,21 @@ export function EpisodeList({ navigation }) {
       >
         <Entypo name="back" size={35} color="#8bcf21" />
       </TouchableOpacity>
+      <View style={styles.viewName}>
+        <Text style={styles.episodeName}>{episode.name}</Text>
+      </View>
+
       <View style={styles.episodeView}>
-        <Text style={styles.episodeText}>Episode List</Text>
+        <View style={styles.episodeInfo}>
+          <Text style={styles.infoText}>Information of episodes</Text>
+        </View>
+        <View style={styles.episodeStyle}>
+          <Text style={styles.episodeText}>{`Id: ${episode.id}`}</Text>
+          <Text
+            style={styles.episodeTitle}
+          >{`Season/Episode: ${episode.episode}`}</Text>
+          <Text style={styles.episodeDate}>{` Date: ${episode.air_date}`}</Text>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -33,16 +54,80 @@ const styles = StyleSheet.create({
   episodeIcon: {
     flex: 1,
     backgroundColor: "#202020",
-    top: 70,
+    justifyContent: "center",
     alignSelf: "flex-start",
     marginLeft: 20,
+    bottom: 60,
+  },
+  viewName: { position: "absolute", alignSelf: "center" },
+  episodeName: {
+    color: "#8bcf21",
+    top: 150,
+    fontFamily: "Creepster_400Regular",
+    fontSize: 33,
+
+    borderWidth: 2,
+    borderColor: "#8bcf21",
+    borderRadius: 10,
+    padding: 10,
+  },
+  episodeInfo: {
+    top: "15%",
+    width: 300,
+    height: 50,
+    backgroundColor: "#202020",
+    justifyContent: "center",
+    alignSelf: "center",
+    borderRadius: 28,
+    borderWidth: 2,
+    borderColor: "#8bcf21",
+  },
+  infoText: {
+    color: "#8bcf21",
+    alignSelf: "center",
+    fontSize: 20,
+    fontFamily: "Creepster_400Regular",
   },
   episodeView: {
-    flex: 3,
+    flex: 2,
     backgroundColor: "#3f3f3f",
     borderTopLeftRadius: 25,
     borderTopRightRadius: 25,
     elevation: 80,
+    borderWidth: 3,
+    borderColor: "#8bcf21",
   },
-  episodeText: { color: "#8bcf21", top: 80, alignSelf: "center" },
+  episodeStyle: {
+    top: "35%",
+    width: 280,
+    height: 200,
+    backgroundColor: "#202020",
+    position: "absolute",
+    alignSelf: "center",
+    justifyContent: "center",
+    borderRadius: 30,
+    borderWidth: 3,
+    borderColor: "#8bcf21",
+  },
+  episodeText: {
+    color: "#8bcf21",
+    alignSelf: "center",
+    padding: 10,
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  episodeTitle: {
+    color: "#8bcf21",
+    alignSelf: "center",
+    padding: 10,
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  episodeDate: {
+    color: "#8bcf21",
+    alignSelf: "center",
+    padding: 10,
+    fontSize: 18,
+    fontWeight: "bold",
+  },
 });
