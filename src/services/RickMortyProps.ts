0a708ad1cdeb2e499d80e0ecef2b38b78166dd87
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-06-12 18:26:18 -0300

    criação interface

---
+++ src/services/RickMortyProps.ts
@@ -0,0 +1,13 @@
+export interface RickMortyProps {
+  id: string;
+  name: string;
+  status: string;
+  species: string;
+  gender: string;
+  image: string;
+
+  air_date: string;
+  episode: string;
+  type: string;
+  dimension: string;
+}
