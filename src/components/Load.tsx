b089c9a6c8ef5eae038078a0593d948a5459de42
Hugo de Oliveira <hugo.lobo47@gmail.com>
2023-06-26 14:14:16 -0300

    criação tela de carregamento

---
+++ src/components/Load.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { ActivityIndicator } from "react-native";
+
+export function Load() {
+  return <ActivityIndicator color="#8bcf21" size="large" style={{ flex: 1 }} />;
+}
